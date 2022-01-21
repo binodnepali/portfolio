14ede626d89cba95a89a7c98b783952770b16644
Binod Nepali <nepalibinod9@gmail.com>
2022-01-21 23:50:15 +0100

    chore: add prettier configuration

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import 'open-props/style';
-import 'open-props/normalize';
+import { defineComponent } from 'vue'
+import 'open-props/style'
+import 'open-props/normalize'
 
-import LinkedInProfileBadge from './components/LinkedInProfileBadge/LinkedInProfileBadge.vue';
+import LinkedInProfileBadge from './components/LinkedInProfileBadge/LinkedInProfileBadge.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     LinkedInProfileBadge,
   },
-});
+})
 </script>
--- src/components/LinkedInProfileBadge/LinkedInProfileBadge.vue
+++ src/components/LinkedInProfileBadge/LinkedInProfileBadge.vue
@@ -11,9 +11,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'LinkedInProfileBadge',
-});
+})
 </script>
