05b5e033e9d499127c23aaa00c3d776fd59e8934
Binod Nepali <nepalibinod9@gmail.com>
2022-03-15 19:40:17 +0100

    chore: remove tailwindcss setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 import { defineNuxtConfig } from 'nuxt3';
 
-import postcssOptions from './postcss.config';
-
 export default defineNuxtConfig({
   css: ['@/styles/global.css'],
-  build: {
-    postcss: {
-      postcssOptions,
-    },
-  },
 });
