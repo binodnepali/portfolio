4d6aa9f219bc7586fba89e237db36f88453b2c68
Binod Nepali <nepalibinod9@gmail.com>
2022-04-08 17:47:41 +0200

    chore: upate head meta description content

--- layouts/AppLayout.vue
+++ layouts/AppLayout.vue
@@ -2,10 +2,10 @@
   <div>
     <Html lang="en-GB">
       <Head>
-        <Title>binodnepali.me</Title>
+        <Title>Binod Nepali - Frontend Software Engineer</Title>
         <Meta
           name="description"
-          content="Fulltime Frontend Software Engineer at adidas. Fond of working with javascript frameworks."
+          content="I am a full-time Frontend Software Engineer and fond of working with Javascript frameworks. I mostly love working with React (a UI library), Vue, Next.js and Nuxt.js frameworks. I am also quite familiar (working) with the Javascript backend frameworks such as Express.js and GraphQL. Always enthusiastic about new web trends. And try to make time to learn and gets my hands dirty."
         />
       </Head>
       <Body lang="en-GB" />
