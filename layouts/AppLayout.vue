76b0c87ddcd1da5ecdf82a04e5d0e80d84142c3f
Binod Nepali <nepalibinod9@gmail.com>
2022-07-19 22:04:37 +0200

    style: fix bio section visibility

--- layouts/AppLayout.vue
+++ layouts/AppLayout.vue
@@ -8,7 +8,6 @@
           content="I am a full-time Frontend Software Engineer and fond of working with Javascript frameworks. I mostly love working with React (a UI library), Vue, Next.js and Nuxt.js frameworks. I am also quite familiar (working) with the Javascript backend frameworks such as Express.js and GraphQL. Always enthusiastic about new web trends. And try to make time to learn and gets my hands dirty."
         />
       </Head>
-      <Body lang="en-GB" />
     </Html>
     <ThemeProvider>
       <header>
--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <main style="height: 100vh" class="main" :class="{ 'main--dark': isDark }">
+  <main class="main" :class="{ 'main--dark': isDark }">
     <slot />
   </main>
 </template>
@@ -10,11 +10,20 @@ const { isDark } = useTheme();
 </script>
 
 <style lang="sass" scoped>
+@use '../styles/breakpoints.scss'
 .main
   background-color: var(--gray-0)
   color: var(--gray-7)
+  height: calc(100% - var(--header-height))
+  padding: var(--size-4)
 
 .main--dark
   background-color: var(--primary-dark-color)
   color: var(--gray-1)
+
+
+@media (min-width: breakpoints.$breakpoint-tablet)
+  .main
+    height: calc(100vh - var(--header-height))
+    padding-top: var(--size-4)
 </style>
