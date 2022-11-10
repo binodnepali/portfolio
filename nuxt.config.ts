14e112290f42bfc843e6cd6a531acf45170d6585
Binod Nepali <nepalibinod9@gmail.com>
2022-11-10 17:40:23 +0100

    feat: add post css jit props to include only used vars in css bundle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 import { defineNuxtConfig } from 'nuxt/config';
 import svgLoader from 'vite-svg-loader';
 
+import OpenProps from 'open-props';
+
 export default defineNuxtConfig({
   vite: {
     plugins: [svgLoader()],
   },
   css: ['@/styles/global.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          'postcss-jit-props': OpenProps,
+        },
+      },
+    },
+  },
 });
