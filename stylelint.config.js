b5a30c27774597f300343ce13e03222257d36c5a
Binod Nepali <nepalibinod9@gmail.com>
2022-11-14 18:56:58 +0100

    chore: add stylelint and add scripts

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-prettier',
+    'stylelint-config-idiomatic-order',
+  ],
+  rules: {},
+}
